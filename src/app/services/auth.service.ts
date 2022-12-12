b1116dff7fbdf898c3f7dca496c34a78fcea9ca5
Vicentetw <75254133+Vicentetw@users.noreply.github.com>
2022-12-12 09:27:15 -0300

    token and auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { Router } from '@angular/router';
 import { JwtDto } from '../entidades/jwt-dto';
 import { LoginUsuario } from '../entidades/login-usuario';
 import { NuevoUsuario } from '../entidades/nuevo-usuario';
@@ -11,8 +12,12 @@ import { NuevoUsuario } from '../entidades/nuevo-usuario';
 })
 export class AuthService {
 URL = environment.apiUrl + 'auth/';
+estaLogeado = false;
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private router:Router
+    ) { }
 
 public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
   return this.httpClient.post<any>(this.URL +'nuevo', nuevoUsuario);
@@ -20,6 +25,12 @@ public nuevo(nuevoUsuario: NuevoUsuario): Observable<any>{
 
 public login(loginUsuario: LoginUsuario): Observable<JwtDto>{
   return this.httpClient.post<JwtDto>(this.URL + 'login', loginUsuario); 
+}
+ //logout y redirect to login
+ SignOut(){
+  this.tokenService.logOut() ;
+  window.location.reload();
+  this.router.navigate(['login']);
 }
 
 }
